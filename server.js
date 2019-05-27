1bdbd4be4dc1a2970471dc712f3dd1768988b4cc
Rachel <rbennett1984@gmail.com>
2019-05-27 06:31:13 -0400

    model js

--- server.js
+++ server.js
@@ -2,12 +2,22 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
+const router = require('./app/router');
+const config = require('./app/config');
 
 const app = express();
-const config = require('./app/config');
+
+app.use(bodyParser.json());
+
+
 app.set('port', config.port);
 app.listen(app.get('port'), err=>{
   if(err) console.error(err);
   console.log(`Server listening on port ${app.get('port')}...`);
+  const db = mongoose.connect(config.db);
+  mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${config.db}`);
+  });
 });
+router(app);
 
